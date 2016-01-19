695fa3ab549013a376d28659f181d301fa2b72bb
anonym0us3 <nathan.shayevich@gmail.com>
2016-01-18 23:14:53 -0800

    create article controller

--- trial2/config/routes.rb
+++ trial2/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'articles/index'
+
+  get 'articles/new'
+
+  get 'articles/create'
+
+  get 'articles/show'
+
   get '/users' => "users#index"
 
   get '/users/new' => "users#new"
